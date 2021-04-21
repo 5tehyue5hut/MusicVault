bf8cb9f680f0cb48212371bf81789f86c69c3318
5tehyue5hut <71716759+5tehyue5hut@users.noreply.github.com>
2021-04-21 13:00:52 +0300

    Add files via upload

---
+++ commands/leave.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../util/error");
+
+module.exports = {
+    info: {
+        name: "leave",
+        aliases: ["goaway", "disconnect"],
+        description: "Leave The Voice Channel!",
+        usage: "Leave",
+    },
+
+    run: async function (client, message, args) {
+        let channel = message.member.voice.channel;
+        if (!channel) return sendError("I'm sorry but you need to be in a voice channel!", message.channel);
+        if (!message.guild.me.voice.channel) return sendError("I Am Not In Any Voice Channel!", message.channel);
+
+        try {
+            await message.guild.me.voice.channel.leave();
+        } catch (error) {
+            await message.guild.me.voice.kick(message.guild.me.id);
+            return sendError("Trying To Leave The Voice Channel...", message.channel);
+        }
+
+        const Embed = new MessageEmbed()
+            .setAuthor("Leave Voice Channel", "https://cdn.discordapp.com/attachments/832397010173689857/834127547666137108/96X96_-_GIF.gif")
+            .setColor("GREEN")
+            .setTitle("Success")
+            .setDescription("🎶 Left The Voice Channel.")
+            .setTimestamp();
+
+        return message.channel.send(Embed).catch(() => message.channel.send("🎶 Left The Voice Channel :C"));
+    },
+};
